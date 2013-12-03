fe206872a4f378c91681447ecb15465aa5496df9
Jason Rushton <github.com@schtuph.com>
2013-12-03 04:35:47 -0700

    explicitly set logging verbosity and view caching for production and dev

--- config/server.js
+++ config/server.js
@@ -3,28 +3,29 @@ module.exports = function (app, express) {
 
     var path = require('path')
 
-    app.configure(function () {
-        app.set('port', process.env.PORT || 3000);
-        app.set('views', path.join(GLOBAL.appRoot,'views'));
-        app.set('view engine', 'jade');
-        //app.set('view cache', false);
-        app.use(express.favicon(path.join(GLOBAL.appRoot, 'public/images/gw2-dragon-32.png')));
-        app.use(express.logger('dev'));
-        app.use(express.bodyParser());
-        app.use(express.methodOverride());
-        app.use(app.router);
-        app.use(express.static(path.join(GLOBAL.appRoot, 'public')));
-    });
-
 
     app.configure('production', function () {
-
+        app.use(express.logger('short'));
+        app.set('view cache', true);
     });
 
 
     app.configure('development', function () {
+        app.use(express.logger('dev'));
         app.use(express.errorHandler());
+        app.set('view cache', false);
         app.locals.pretty = true;
+    });
+
+    app.configure(function () {
+        app.set('port', process.env.PORT || 3000);
+        app.set('views', path.join(GLOBAL.appRoot,'views'));
+        app.set('view engine', 'jade');
+        app.use(express.favicon(path.join(GLOBAL.appRoot, 'public/images/gw2-dragon-32.png')));
+        app.use(express.bodyParser());
+        app.use(express.methodOverride());
+        app.use(app.router);
+        app.use(express.static(path.join(GLOBAL.appRoot, 'public')));
     });
 
 
