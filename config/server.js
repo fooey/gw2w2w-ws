0bbc41dd5528730c0fcfe1ce8eff77cacfd2cd62
Jason Rushton <github.com@schtuph.com>
2014-01-09 23:36:52 -0700

    tabs for indent

--- config/server.js
+++ config/server.js
@@ -1,37 +1,39 @@
 module.exports = function (app, express) {
-    var config = this;
+	const config = this;
 
-    var path = require('path')
-
-
-
-    app.configure(function () {
-        app.set('port', process.env.PORT || 3000);
-
-        app.set('views', path.join(process.cwd(),'views'));
-        app.set('view engine', 'jade');
-        app.set('view cache', true);
-
-        app.use(express.favicon(path.join(process.cwd(), 'public/images/gw2-dragon-32.png')));
-        app.use(express.urlencoded());
-        app.use(express.methodOverride());
-        app.use(app.router);
-        app.use(express.static(path.join(process.cwd(), 'public')));
-
-        app.use(express.logger('dev'));
-    });
-
-    app.configure('development', function(){
-        app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-        app.locals.pretty = true;
-    });
-
-    app.configure('production', function(){
-        app.use(express.errorHandler());
-    });
+	const path = require('path')
+	const pubFolder = path.join(process.cwd(), 'public');
+	const faviconPath = path.join(pubFolder, 'images/gw2-dragon-32.png');
 
 
 
+	app.configure(function () {
+		app.set('port', process.env.PORT || 3000);
 
-    return config;
+		app.set('views', path.join(process.cwd(),'views'));
+		app.set('view engine', 'jade');
+		app.set('view cache', true);
+
+		app.use(express.favicon(faviconPath));
+		app.use(express.urlencoded());
+		app.use(express.methodOverride());
+		app.use(app.router);
+		app.use(express.static(pubFolder));
+
+		app.use(express.logger('dev'));
+	});
+
+	app.configure('development', function(){
+		app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+		app.locals.pretty = true;
+	});
+
+	app.configure('production', function(){
+		app.use(express.errorHandler());
+	});
+
+
+
+
+	return config;
 };
