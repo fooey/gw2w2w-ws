8f446c3828f144058f70233a8345595662e9c314
Jason Rushton <github.com@schtuph.com>
2014-01-09 23:41:40 -0700

    route for dumping data to the browser

--- routes/data.js
+++ routes/data.js
@@ -1,10 +1,44 @@
-var data = require('../lib/dataHandler.js');
+module.exports = function (req, res) {
+	const dataType = req.params.dataType;
+	const extension = req.params.extension;
+	const matchId = req.params.matchId;
+
+	if(dataType === 'worlds'){
+		require('../lib/worlds').getWorlds(returnResults); 
+	}
+	else if(dataType === 'objectives'){
+		require('../lib/objectives').getObjectives(returnResults); 
+	}
+	else if(dataType === 'objectiveGroups'){
+		returnResults(null, require('../lib/objectiveGroups').getByMap());
+	}
+	else if(dataType === 'objectiveTypes'){
+		returnResults(null, require('../lib/objectiveTypes').objectiveTypes);
+	}
+	else if(dataType === 'matches'){
+		require('../lib/matches').getMatches(returnResults); 
+	}
+	else if(dataType === 'scores'){
+		require('../lib/matchDetails').getScores(returnResults); 
+	}
+	else if(dataType === 'matchDetails'){
+		data = require('../lib/matchDetails').getById(matchId, returnResults); 
+	}
+	else if(dataType === 'state'){
+		data = require('../lib/objectiveState').getById(matchId, returnResults); 
+	}
+	else{
+		res.status(404).send('Not found');
+	}
 
 
-module.exports.updateData = function (req, res) {
-    data.init(req, res)
+	function returnResults(err, data){
+		if(extension === 'json')
+			res.json(data)
+		else if(extension === 'js') 
+			res.send(dataType + '=' + JSON.stringify(data) )
+		else
+			res.status(404).send('Not found');
+	}
 
-    data.updateData();
-
-    res.send(GLOBAL.GW2.timeStamps);
 };
